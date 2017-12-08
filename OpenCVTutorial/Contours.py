9a4505fad8e2a7a089b7ec0655d109080886bb7f
sophieyu258 <sophieyu258@yahoo.ca>
2017-12-07 22:33:01 -0500

    Match Shapes
    http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_contours/py_contours_more_functions/py_contours_more_functions.html

--- OpenCVTutorial/Contours.py
+++ OpenCVTutorial/Contours.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-dir = 'D:\\hbwork\\pyprojects\\data\\'
+dir = 'C:\\hbwork\\pyprojects\\\OpenCVTutorial\\data\\'
 
 img = cv2.imread(dir + '17.png')
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -36,10 +36,30 @@ def Test2():
 
     cv2.imshow('img',img)
 
+def TestMatchShapes():
+    img1 = cv2.imread(dir + 'star.jpg',0)
+    img2 = cv2.imread(dir + 'star2.jpg',0)
+    img3 = cv2.imread(dir + 'block.jpg',0)
+
+    ret, thresh = cv2.threshold(img1, 127, 255,0)
+    ret, thresh2 = cv2.threshold(img2, 127, 255,0)
+    ret, thresh3 = cv2.threshold(img3, 127, 255,0)
+    image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
+    cnt1 = contours[0]
+    image, contours, hierarchy = cv2.findContours(thresh2,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
+    cnt2 = contours[0]
+    image, contours, hierarchy = cv2.findContours(thresh3,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
+    cnt3 = contours[0]
+
+    ret1 = cv2.matchShapes(cnt1,cnt1,1,0.0)
+    ret2 = cv2.matchShapes(cnt1,cnt2,1,0.0)
+    ret3 = cv2.matchShapes(cnt1,cnt3,1,0.0)
+    print(ret1, ret2, ret3)
+
 if __name__ == "__main__":
     cv2.imshow('img',img)
 
-    Test2()
+    TestMatchShapes()
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
