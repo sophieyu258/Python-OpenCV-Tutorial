600ec4291531f07c9668dd93b6da05c652eb5d3d
bhang <bhang@yahoo.com>
2018-04-18 17:44:44 -0400

    examples from another article

--- OpenCVTutorial/RESTServerTest1.py
+++ OpenCVTutorial/RESTServerTest1.py
@@ -1,12 +1,87 @@
 
 #!/usr/bin/python3
 from flask import Flask, request, jsonify
+from flask import abort
+from flask import make_response
+from flask import request
 from flask_restful import Resource, Api
 from json import dumps
 
 app = Flask(__name__)
 api = Api(app)
 
+# examples from https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+
+
+# examples from https://www.codementor.io/sagaragarwal94/building-a-basic-restful-api-in-python-58k02xsiq
 
 class Employees(Resource):
     def get(self):
@@ -15,10 +90,10 @@ class Employees(Resource):
     def post(self):
         """
         {
-	"LastName":"micheal",
-"FirstName":"jackson",
-"Title":"CEO"
-}
+	    "LastName":"micheal",
+        "FirstName":"jackson",
+        "Title":"CEO"
+        }
         """
         print(request.json)
         LastName = request.json['LastName']
