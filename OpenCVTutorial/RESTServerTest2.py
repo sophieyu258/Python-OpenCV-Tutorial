7dae0f406f062740913857b39c62a295bcf3134e
sophieyu258 <sophieyu258@yahoo.ca>
2018-04-24 23:28:59 -0400

    receive picture from Camera2Basic (working!)

--- OpenCVTutorial/RESTServerTest2.py
+++ OpenCVTutorial/RESTServerTest2.py
@@ -28,10 +28,10 @@ def allowed_file(filename):
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
+        #if 'file' not in request.files:
+        #    flash('No file part')
+        #    return redirect(request.url)
+        file = request.files['myfile']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
